4193899dbc2278bb678fc6b2a32a9dbdf37f098b
Victor Oliveira <victor.souza.41@hotmail.com>
2020-06-23 19:32:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		
 		
 		Seller seller = new Seller (21, "Victor", "victor@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDAO sellerDao = DaoFactory.createSellerDao(); 
 		
 		System.out.println(seller);
 
